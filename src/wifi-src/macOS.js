6789a219db278305554122af67a7420ad52a59ec
Jakub Synowiec <github@jakubsynowiec.info>
2018-11-14 11:41:50 +0100

    Change RE to case-insensitive

--- src/wifi-src/macOS.js
+++ src/wifi-src/macOS.js
@@ -3,7 +3,7 @@ const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 
 function parseAirportAccessPoints(str) {
-  const MAC_RE = /(?:[\da-f]{2}[:]{1}){5}[\da-f]{2}/;
+  const MAC_RE = /(?:[\da-f]{2}[:]{1}){5}[\da-f]{2}/i;
 
   return str.split('\n').reduce((acc, line) => {
     const mac = line.match(MAC_RE);
